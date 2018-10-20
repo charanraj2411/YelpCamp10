1751884274b16f0f1dab1765f9b9ea35f9b19f90
Charanraj Shetty <charanrajshetty1993@gmail.com>
2018-10-20 20:04:15 +0530

    v10

---
+++ routes/comments.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var router = express.Router({mergeParams: true});
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+
+//comments new
+router.get("/new", isLoggedIn, function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+//Comments create
+router.post("/",isLoggedIn, function(req, res){
+    //loookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                }else{
+                    //add username and is to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    //save comment
+                    comment.save();
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/'+campground._id);
+                }
+            });
+        }
+    });
+});
+
+//COMMENTS EDIT ROUTE
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.rdirect("back");
+        }else{
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    res.render("comments/edit", {campground_id: req.params.id});
+
+        });            
+});
+
+//COMMENTS UPDATE
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updateComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/campgrounds" + req.params.id);
+        }
+    });
+});
+
+//COMMENT DESTROY ROUTE
+router.delete(":/comment_id", function(req, res){
+    //findByIdAndRemove
+    res.send("THIS IS THE DESTROY COMMENT ROUTE!");
+})
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
